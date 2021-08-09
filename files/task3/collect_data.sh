529ced0739a532ee39b4d5db7a23e3bb0998db22
Alaa_Bishtawi <bish.h.alaa@gmail.com>
2021-08-09 09:03:28 -0400

    Seconed Commit

--- files/task3/collect_data.sh
+++
@@ -1,5 +0,0 @@
-cpu=$(mpstat |grep -i all| awk '{sum=$3+$4+$5+$6+$7+$8+$9+$10+$11} END {print sum }')
-memory=$(free|grep Mem|awk '{print $3}')
-disk=$(df --total|grep -i total|awk '{print $3 }')
-ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-sql-container)
-mysql -ualaa -p123 -h $ip -e "use test1;INSERT INTO dataa (time, cpu,ram,disk) VALUES (CURRENT_TIMESTAMP(),$cpu,$memory,$disk);"
